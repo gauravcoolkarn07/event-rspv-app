1ad85eadc5db2dd3474231f59bf286d9f9739727
gauravcoolkarn07 <gauravcoolkarni07@gmail.com>
2026-02-27 23:01:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ def rsvp(event_id):
 
     return redirect('/')
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
